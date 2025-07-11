5c44e729cd967219bcaff0174b1acb8e1bd1142a
remi.bezot <6487359+remi-bezot@users.noreply.github.com>
2025-07-11 23:16:20 +0200

    Revert "chore: add tests and review text image and timeseries in preprocessing"
    This reverts commit 19fff5dea76004ecaf969f7bb653f0d63098ffe6.

--- src/data/preprocessing/test_text.rs
+++ src/data/preprocessing/test_text.rs
@@ -1,64 +1,48 @@
-// Tests unitaires pour text.rs
+#[cfg(test)]
+mod tests {
+    use super::clean_texts;
+    use super::embed_texts;
+    use super::preprocess_texts;
 
-use crate::data::preprocessing::text::*;
-
-mod clean_texts_tests {
-    use super::*;
     #[test]
-    fn test_basic() {
+    fn test_preprocess_texts_basic() {
+        let texts = vec![String::from("hello world")];
+        let vecs = preprocess_texts(&texts);
+        assert_eq!(vecs[0], vec![5.0, 5.0]);
+    }
+
+    #[test]
+    fn test_clean_texts_basic() {
         let texts = vec![String::from("Hello, World!")];
         let cleaned = clean_texts(&texts);
         assert_eq!(cleaned[0], "hello world");
     }
+
     #[test]
-    fn test_empty() {
+    fn test_clean_texts_empty() {
         let texts = vec![String::from("")];
         let cleaned = clean_texts(&texts);
         assert_eq!(cleaned[0], "");
     }
+
     #[test]
-    fn test_punctuation() {
+    fn test_clean_texts_punctuation() {
         let texts = vec![String::from("C'est, vraiment! génial?")];
         let cleaned = clean_texts(&texts);
         assert_eq!(cleaned[0], "cest vraiment genial");
     }
-}
 
-mod tokenize_text_tests {
-    use super::*;
     #[test]
-    fn test_basic() {
-        let txt = "Hello world";
-        assert_eq!(tokenize_text(txt), vec!["Hello", "world"]);
-    }
-    #[test]
-    fn test_empty() {
-        assert_eq!(tokenize_text("").len(), 0);
-    }
-}
-
-mod embed_texts_tests {
-    use super::*;
-    #[test]
-    fn test_basic() {
+    fn test_embed_texts_basic() {
         let texts = vec![String::from("hello world")];
         let embedded = embed_texts(&texts);
         assert_eq!(embedded[0], vec![10.0]);
     }
+
     #[test]
-    fn test_empty() {
+    fn test_embed_texts_empty() {
         let texts = vec![String::from("")];
         let embedded = embed_texts(&texts);
         assert_eq!(embedded[0], vec![0.0]);
-    }
-}
-
-mod preprocess_texts_tests {
-    use super::*;
-    #[test]
-    fn test_basic() {
-        let texts = vec![String::from("hello world")];
-        let vecs = preprocess_texts(&texts);
-        assert_eq!(vecs[0], vec![5.0, 5.0]);
     }
 }
